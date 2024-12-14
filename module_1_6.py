42b95021264b5e3640447d56c8afd3dec2083a06
anmak777 <makhnytc@gmail.com>
2024-12-14 09:26:27 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,12 @@
+home_work_count_total = 12
+
+hour_count_total = 1.5
+
+course_title = 'Python'
+
+hour_count_avg = hour_count_total/home_work_count_total
+
+print('Курс:',course_title,', всего задач:',home_work_count_total,
+      ', потрачено часов:',hour_count_total,', среднее время выполнения:',hour_count_avg,
+      sep='')
+
