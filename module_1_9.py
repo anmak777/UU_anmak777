8e398633be69e3e1c1d96384be7d75e117147c1e
anmak777 <makhnytc@gmail.com>
2024-12-14 09:34:00 +0300

    Add files via upload

---
+++ module_1_9.py
@@ -0,0 +1,10 @@
+example = 'Программирование'
+print(example[0])
+print(example[-1])
+print(example[9:])
+print(example[::-1])
+print(example[1::2])
+
+print(len(example))
+print(len(example)//2)
+print(example[(len(example)-1)//2:])
