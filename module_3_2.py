5fb8a139a164c81650165a97c95a1f880496d7be
anmak777 <makhnytc@gmail.com>
2024-12-27 20:21:17 +0300

    Add files via upload

---
+++ module_3_2.py
@@ -0,0 +1,15 @@
+def send_email(message,recipient,*,sender = 'university.help@gmail.com'):
+    if '@' not in recipient or '@' not in sender or recipient.endswith(('.com','.ru','.net')) == False or sender.endswith(('.com','.ru','.net')) == False:
+        print('Невозможно отправить письмо с адреса', sender,'на адрес', recipient)
+    elif recipient == sender:
+        print('Нельзя отправить письмо самому себе!')
+    elif sender != 'university.help@gmail.com':
+        print('НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса', sender,'на адрес', recipient)
+    else:
+        print('Письмо успешно отправлено с адреса', sender,'на адрес', recipient)
+
+
+send_email('Это сообщение для проверки связи','vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!','urban.fan@gmail.com',sender = 'urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
